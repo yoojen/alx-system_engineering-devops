ba623fb63b0849bd16edd63bef15e6995fb01cd3
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-25 21:17:57 +0200

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { 'school':
+#create file and write something
+
+file { '/tmp/school':
   ensure  => 'present',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school',
+  owner   => 'www-data'
 }
