a513608116540f05621a0aca90b266535fd68edd
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-27 22:18:47 +0200

    change file to exec command

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #make ssh config file using puppet
-file { 'ssh_config':
+exec { 'ssh_config':
   path => '/bin/',
   command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config';
 }
