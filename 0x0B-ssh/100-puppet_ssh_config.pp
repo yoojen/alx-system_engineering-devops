0f41992828d4e3c61f5ff4679cc26c7f77aca4d1
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-27 22:21:22 +0200

    updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
-#make ssh config file using puppet
+# make ssh config file using puppet
 exec { 'ssh_config':
   path => '/bin/',
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config';
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
 }
