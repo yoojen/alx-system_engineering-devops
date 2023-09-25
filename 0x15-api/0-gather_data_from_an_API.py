f2bdb65b34aac84ba9a0d3b97e84245fb051ee91
Eugene <140322568+yoojen@users.noreply.github.com>
2023-09-26 00:13:10 +0200

    first tech if api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""request through api and show on the stout"""
+import sys
+import requests
+
+
+url = "https://jsonplaceholder.typicode.com/"
+home = requests.get(url)
+todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+user = requests.get(url + "users", params={"id": sys.argv[1]}).json()
+taskTitle = [todo.get("title") for todo in todos if todo.get("completed") == True]
+
+for one in user:
+name = one.get("name")
+print("Employee  {} is done with task({}/{})".format(name, len(taskTitle), len(todos)))
+[print("\t{}".format(todo.get("title"))) for todo in todos if todo.get("completed") == True]
