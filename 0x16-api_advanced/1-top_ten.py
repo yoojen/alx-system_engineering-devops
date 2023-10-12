ca4dca285a212251220d18024732fe351b9171c5
eugene <eugeneemma7@gmail.com>
2023-10-12 15:41:46 -0700

    added

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,12 +8,12 @@ def top_ten(subreddit):
     the first 10 hot posts listed for a given subreddit.
     """
 
-    response = requests.get('https://api.reddit.com/r/{}/hot?limit=10'
-                            .format(subreddit),  allow_redirects=False)
-
-    if response.status_code == 200:
-        subreddit_info = response.json()
-        for post in subreddit_info['data']['children']:
-            print(post['data']['title'])
-    else:
+    response = requests.get('https://api.reddit.com/r/{}/hot/.json'
+                            .format(subreddit),  params={"limit": 10}, allow_redirects=False)
+     
+    if response.status_code != 200:
         print(None)
+    else:
+        subreddit_info = response.json().get("data")
+        for sub in subreddit_indo.get("children"):
+            print(sub.get("data").get("title"))
