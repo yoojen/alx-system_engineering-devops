f4d3ea4b8df6adf6d318c919fe3c23f2545bdf53
Eugene <140322568+yoojen@users.noreply.github.com>
2023-10-21 21:43:18 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,8 +3,8 @@
 #increasing ulimit of the server
 
 exec { 'fix-nginx':
- command => 'sed -i "s/15/4096/ /etc/default/nginx",
- path => '/usr/bin/:/bin'
+ command => 'sed -i "s/15/4096/" /etc/default/nginx',
+ path => '/usr/local/bin/:/bin'
 }
 
 #restartgin nginx
